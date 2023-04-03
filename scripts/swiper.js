a033cf658c5aaf571caa2e38050b992a3e8903b7
Stanislav <90844565+kkkovalchukkk@users.noreply.github.com>
2023-04-03 23:44:43 +0700

    Add files via upload

---
+++ scripts/swiper.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 6,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
